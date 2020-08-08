f1ca9a1e8b57d5d292a0964311d082c08dd136fc
Natalie <natalierodri96@gmail.com>
2020-08-08 10:53:02 -0500

    fifth commit

--- script.js
+++ script.js
@@ -4,76 +4,73 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789");
+  var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 
 var confirmLength;
-var confirmUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-var uppArr = confirmUpper.split("");
-var confirmLower = "abcdefghijklmnopqrstuv"
-var lowArr = confirmLower.split("");
+var confirmUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var upperArr = confirmUpper.split("");
+var confirmLower = "abcdefghijklmnopqrstuvwxyz";
+var lowerArr = confirmLower.split("");
 var confirmNumber = "1234567890";
-var numbArr = confirmNumber.split("");
-var confirmChar = "!@#$%^&*()_+=:;'{()}~/,.<>"
+var numberArr = confirmNumber.split("");
+var confirmChar = "~!@#$%^&*(_)=+-[\]{|};:,./?";
 var charArr = confirmChar.split("");
 var confirmPrompt = [];
-var chParameters = [];
+var charParameters = [];
 
-function generatePassword() {
+function generatePassword () {
 
-  var confirmLength = (prompt("Choose password length between 8-128"));
-  
+  var confirmLength = (prompt("Choose between 8-128 characters"));
   if (confirmLength >= 8 && confirmLength <=128) {
-    alert("Password will contain" + confirmLength + "  characters.");
-    console.log(confirmLength);
+    alert("Password will contain" + confirmLength + "characters");
   } else {
     alert("Select between 8-128");
     return;
   }
-  confirmUpper = confirm("OK to confirm UPPERCASE characters");
 
+  confirmUpper = confirm("OK to confirm uppercase characters");
   if (confirmUpper === true) {
-    chParameters = chParameters.concat("uppercase");
+    charParameters = charParameters.concat("uppercase");
     console.log("uppercase");
-    confirmPrompt = confirmPrompt.concat(uppArr);
+    confirmPrompt = confirmPrompt.concat(upperArr);
   }
-  
+   
   confirmLower = confirm("OK to confirm lowercase characters");
   if (confirmLower === true) {
-    chParameters = chParameters.concat("lowercase");
+    charParameters = charParameters.concat("lowercase");
     console.log("lowercase");
-    confirmPrompt = confirmPrompt.concat(lowArr);
+    confirmPrompt = confirmPrompt.concat(lowerArr);
   }
+ confirmNumber = confirm ("Ok to confirm number");
+ if (confirmNumber === true) {
+   charParameters = charParameters.concat("numbers");
+   console.log("numbers");
+   confirmPrompt = confirmPrompt.concat (numberArr);
+ }
 
-confirmNumber = confirm("OK to confirm numbers");
-if (confirmNumber === true) {
-  chParameters =chParameters.concat("numbers");
-  console.log("numbers");
-  confirmPrompt = confirmPrompt.concat(numbArr)
+ confirmChar = confirm("OK to confirm characters");
+ if (confirmChar === true) {
+   charParameters = charParameters.concat("characters");
+   console.log("characters");
+   confirmPrompt = confirmPrompt.concat(charArr);
+ }
+
+ if (confirmUpper === false && confirmLower === false && confirmNumber === false && confirmChar === false) {
+ alert("please select at least one option");
+ return;
+ } else {
+   alert("Password will contain" + charParameters + ".");
 }
-
-confirmChar = confirm("OK to confirm characters");
-if (confirmChar ===true) {
-  chParameters = chParameters.concat("characters");
-  console.log("characters");
-  confirmPrompt = confirmPrompt.concat(charArr);
+var password ="";
+for (var i = 1; i <= confirmLength; i++) {
+  var randomPass = confirmPrompt[Math.floor(Math.random() * confirmPrompt.length)];
+  password = password + randomPass;
+  return password;
 }
-
-if (confirmUpper === false && confirmLower === false && 
-  confirmNumber === false && confirmChar === false) {
-    alert("Please select at least one option");
-    return;
-  } else {
-    alert("Password will contain" + chParameters + ".");
-  }
-
-  var password = "";
-    for (var i = 1; i <= confirmLength; i++) {
-     var randomPass = confirmPrompt[Math.floor(Math.random()* confirmPrompt.length)];
-      password = password + randomPass;
 }
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
