47310a08a4cec717ee9df18dbac7ef80e0fdf3b7
Natalie <natalierodri96@gmail.com>
2020-08-08 16:20:26 -0500

    sixth commit

--- script.js
+++ script.js
@@ -12,20 +12,14 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
-
-var number = ["1","2","3","4","5","6","7","8","9","0"];
-var characters = ["~","!","@","#","$",",","%","^","&","*","(","_",")","=","+","-","[","]","|","}",";",":",".","/","?","{","}"];
+var confirmLength;
 var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-
-
-
-var confirmLength;
+var confirmNumber = ["1,2,3,4,5,6,7,8,9,0"];
+var confirmChar = ["~","!","@","#","$",",","%","^","&","*","(","_",")","=","+","-","[","]","|","}",";",":",".","/","?","{","}"];
 var upperArr = confirmUpper.split("");
 var lowerArr = confirmLower.split("");
-var confirmNumber = ["1,2,3,4,5,6,7,8,9,0"];
 var numberArr = confirmNumber.split("");
-var confirmChar = ["~","!","@","#","$",",","%","^","&","*","(","_",")","=","+","-","[","]","|","}",";",":",".","/","?","{","}"];
 var charArr = confirmChar.split("");
 var confirmPrompt = [];
 var charParameters = [];
